7855073a6c25601f0a6879bdc2c1dab6f084bd55
jeremy-pdh <jeremy.roche@panoramicdigitalhealth.com>
2026-03-22 18:19:13 +0100

    Keep pushing

---
+++ .zshrc
@@ -0,0 +1,180 @@
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+
+# Path to your Oh My Zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time Oh My Zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="robbyrussell"
+
+# Set list of themes to pick from when loading at random
+# Setting this variable when ZSH_THEME=random will cause zsh to load
+# a theme from this variable instead of looking in $ZSH/themes/
+# If set to an empty array, this variable will have no effect.
+# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+
+# Uncomment the following line to use case-sensitive completion.
+# CASE_SENSITIVE="true"
+
+# Uncomment the following line to use hyphen-insensitive completion.
+# Case-sensitive completion must be off. _ and - will be interchangeable.
+# HYPHEN_INSENSITIVE="true"
+
+# Uncomment one of the following lines to change the auto-update behavior
+# zstyle ':omz:update' mode disabled  # disable automatic updates
+# zstyle ':omz:update' mode auto      # update automatically without asking
+# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+
+# Uncomment the following line to change how often to auto-update (in days).
+# zstyle ':omz:update' frequency 13
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+DISABLE_MAGIC_FUNCTIONS="true"
+
+# Uncomment the following line to disable colors in ls.
+# DISABLE_LS_COLORS="true"
+
+# Uncomment the following line to disable auto-setting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# You can also set it to another string to have that shown instead of the default red dots.
+# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
+# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
+# COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# You can set one of the optional three formats:
+# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# or set a custom format using the strftime function format specifications,
+# see 'man strftime' for details.
+# HIST_STAMPS="mm/dd/yyyy"
+
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='nvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch $(uname -m)"
+
+# Set personal aliases, overriding those provided by Oh My Zsh libs,
+# plugins, and themes. Aliases can be placed here, though Oh My Zsh
+# users are encouraged to define aliases within a top-level file in
+# the $ZSH_CUSTOM folder, with .zsh extension. Examples:
+# - $ZSH_CUSTOM/aliases.zsh
+# - $ZSH_CUSTOM/macos.zsh
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+alias ls='lsd'
+
+# Youtube download
+alias yt-mp3='yt-dlp --extract-audio --audio-format mp3'
+alias yt='yt-dlp'
+
+# Fuzzy finder CTRL+T
+source <(fzf --zsh)
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# jenv
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
+
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
+. "$HOME/.local/bin/env"
+
+# The following lines have been added by Docker Desktop to enable Docker CLI completions.
+fpath=(/Users/jeremy/.docker/completions $fpath)
+autoload -Uz compinit
+compinit
+# End of Docker CLI completions
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/homebrew/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/homebrew/Caskroom/miniconda/base/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+# Zoxide
+eval "$(zoxide init zsh)"
+# pnpm
+export PNPM_HOME="/Users/jeremy/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+# Starship
+eval "$(starship init zsh)"
+
+# Angular
+# Load Angular CLI autocompletion.
+source <(ng completion script)
+
+# Embeded software STM32CubeIDE
+export GNU_TOOLS_FOR_STM32="/Applications/STM32CubeIDE.app/Contents/Eclipse/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.14.3.rel1.macosaarch64_1.0.0.202602081740/"
+export NRF5_SDK="/opt/nordic/nRF5_SDK"
+export STM32_PROGRAMMER_CLI_BIN="/Applications/STM32CubeIDE.app/Contents/Eclipse/plugins/com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer.macosaarch64_1.0.0.202601242230/tools/bin/STM32_Programmer_CLI"
+export EM_BUILD_BIN="/Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.32a/bin/emBuild"
+alias embuild="$EM_BUILD_BIN"
+
+# export JAVA_HOME=/opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk/Contents/Home
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_SDK_ROOT=$ANDROID_HOME
+export PATH=$PATH:$ANDROID_HOME/build-tools/37.0.0-rc2 # Add the build-tools to the PATH for StudyUserApp
+
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools
---
+++ mac-setup.sh
@@ -0,0 +1,240 @@
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew tap rafaelswi/menubarusb # Tap the repository for menubarusb
+
+brew install --cask visual-studio-code \
+                    android-platform-tools \
+                    gcloud-cli \
+                    miniconda \
+                    menubarusb \
+                    handy \
+                    finetune
+
+
+brew install    act \
+                bat \
+                btop \
+                fastlane \
+                fd \
+                ffmpeg \
+                fx \
+                fzf \
+                gcc \
+                gdb \
+                googletest \
+                srecord \
+                gradle@8 \
+                imagemagick \
+                imagesnap \
+                ios-deploy \
+                jasper \
+                jq \
+                llvm \
+                lsd \
+                maven \
+                mole \
+                mycli \
+                ncdu \
+                nvm \
+                pandoc \
+                pillow \
+                protobuf@29 \
+                mysql \
+                mysql-client \
+                rbenv \
+                scrcpy \
+                snappy \
+                stlink \
+                tree \
+                watchman \
+                wget \
+                xz \
+                yt-dlp \
+                zstd \
+                zoxide \
+                zbar \
+                jenv \
+                pyenv \
+                make \
+                openjdk@17 \
+                openjdk@21 \
+                openjdk@25 \
+                cloud-sql-proxy \
+                arm-none-eabi-gdb \
+                starship \
+                cocoapods \
+                zbar
+
+brew link --overwrite --force protobuf@29 # Link specific version
+
+# nvm config
+mkdir ~/.nvm
+nvm install --lts
+nvm use --lts
+
+# pnpm config
+curl -fsSL https://get.pnpm.io/install.sh | sh
+
+# Node.js global packages
+pnpm install -g @angular/cli
+pnpm i -g @ionic/cli
+pnpm i -g native-run
+pnpm i -g md-to-pdf
+npm i -g cordova@10 # Not working with pnpm
+
+# rbenv config
+rbenv init
+rbenv install 2.7.5
+rbenv install 4.0.2
+rbenv global 4.0.2
+
+# pyenv config
+pyenv install 3.13
+pyenv install 2
+pyenv global 3.13
+
+# jenv
+jenv add /opt/homebrew/opt/openjdk@17
+jenv add /opt/homebrew/opt/openjdk@25
+jenv add /opt/homebrew/opt/openjdk@21
+jenv global 25
+jenv doctor # Check for any issues with jenv setup
+
+# gcloud cli
+gcloud auth login
+gcloud auth application-default login
+
+# uv install
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# conda setup
+conda init "$(basename "${SHELL}")"
+
+# Xcode setup
+# Install Xcode from the App Store, then run this command to set the path
+sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+
+# Generate SSH key and add to GitHub
+ssh-keygen
+
+# Clone repos and setup projects
+git clone git@github.com:panoramic-digital-health/beapp.git
+git clone git@github.com:jeremy-pdh/embedded-software.git
+
+# Configure zbar (QR code scanning library)
+ln -s $(brew --prefix zbar)/lib/libzbar.dylib ~/lib/libzbar.dylib
+
+# Ollama setup
+curl -fsSL https://ollama.com/install.sh | sh
+ollama run gemma3:27b # Install gemma3:27b model (light local Gemini)
+
+# Android SDK
+sdkmanager "platform-tools" "platforms;android-32" "build-tools;32.0.0" # TODO: I think it's legacy
+sdkmanager --licenses
+
+# Reduce spacing in the menu bar
+defaults -currentHost write -globalDomain NSStatusItemSpacing -int 6
+defaults -currentHost write -globalDomain NSStatusItemSelectionPadding -int 6
+
+# System Settings
+# --------
+# Set language in English
+# Battery:
+#   On Battery: Automatic
+#   Power Adapter: High Power
+# Docks: Automatically hide and show the Dock (I never use it)
+# Menu bar from left to right:
+#   MenubarUSB, Docker, Maccy, Handy, TRex, Stats (CPU, GPU, Memory, Power), WiFi, Battery, AlDente, Control Center, HH:MM:SS
+# Keyboard:
+#   Turn keyboard backlight off after 5 seconds of inactivity
+#   Keyboard Shortcuts:
+#     Keyboard: Move focus to next window : ⌘ + @
+#     Spotlight: Disable all (use Raycast instead)
+#     Function Keys: Enable to use F1, F2, etc. as standard function keys
+# Trackpad:
+#   Tap to click: Enabled
+
+# Apps and tools
+# --------
+# Dev tools
+#   VSCode : code editor [installed with brew]
+#   Android Studio: Android development IDE
+#   Xcode : iOS and macOS development IDE
+#   TestFlight : iOS app testing and distribution
+#   STM32CubeIDE : STM32 microcontroller development IDE
+#   Segger Embedded Studio for ARM 7.32a : embedded development IDE
+#   Docker Desktop : containerization and development
+#   iTerm2 : terminal, install Hack Nerd Font for better icons and ligature
+#   Insomnia : API testing and development
+#   nRF Connect for Desktop : Nordic Semiconductor development tool
+#   Ollama : local LLM hosting and management
+#   MySQL Server: Local MySQL server for development and testing (https://dev.mysql.com/doc/refman/8.4/en/macos-installation.html)
+#
+#
+# Utilities
+#   Raycast : Productivity launcher and automation
+#   MenubarUSB : Monitor USB devices in the menu bar [installed with brew]
+#   Maccy : Clipboard history (⌘ + ⇧ + C)
+#   Handy : Offline speech-to-text (⌥ + Space) [installed with brew]
+#   TRex : Copy any text on your screen, OCR
+#   Stats : System monitoring in the menu bar
+#   AlDente : Battery health management
+#   Iriun Webcam : Use phone as webcam
+#   LosslessCut : Simple video editor for cutting and merging without re-encoding
+#   PeekX : Quick Look Extension for Folder and Markdown
+#   QuickRecorder : Screen recording
+#   HiddenBar: Hide/Manage menu bar icons
+#
+#
+# Others
+#   Microsoft Teams : Work communication
+#   Beeper : Universal messaging app
+#   Davinci Resolve : Video editing software
+#   Brave : Web browser (for personal use)
+#     Extensions: Dashlane (Password manager), Dark Reader, Url shortener, YouTube Playback speed, SponsorBlock,
+#   Chrome : Web browser (for work tasks)
+#     Extensions: Angular DevTools, Bitwarden (Password manager)
+#   Hotsport Shield : VPN
+#
+#
+# Raycast :
+#   Extensions:
+#     Color Picker
+#     Clipboard History
+#     Copy Path or URL
+#     GitHub
+#     Google Tranlate
+#     Identify Song
+#     Image Modification
+#     Kill Process
+#     Media converter (ffmpeg wrapper)
+#     Navigation
+#     Port Manager
+#     Quicklinks
+#     Raycast Ai
+#     Search Emoji & Symbols: ⌃ + ⌘ + Space
+#     Search Files File Search: ⌥ + Space
+#     Search for GIFs GIF Search
+#     Video Downloader
+#     Visual Studio Code
+#     YAFW (Video compression)
+#   Snippets:
+#     !opt: ⌥ ⌘
+#     !cmd: ⌘
+#     !shift: ⇧
+#     !ctrl: ⌃
+#     !date: MMM DD, YYYY
+#     !time: HH:MM
+#     @@ : my@email.com
+#   Quicklinks:
+#     VSCode main projects (eg Backend : code ~/beapp/backend)
+#     Youtube search : ⌥ + Y
+#     Applications: Open Applications/ in Finder
+#     Downloads: Open Downloads/ in Finder
+#     inspect devices: open chrome://inspect/#devices in Chrome
